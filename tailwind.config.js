ec2dd9e2aa5a611b62e3350bb762f96fdfff0822
MDafaAdiwinata <dafaadi2106@gmail.com>
2026-06-06 11:56:55 +0700

    feat(home): home page created succesfully

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,32 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./node_modules/flowbite/**/*.js", "./**/*.{html,js}"],
+  content: ["./node_modules/flowbite/**/*.js", "./*.html", "./*.js"],
   theme: {
-    extend: {},
+    extend: {
+      keyframes: {
+        marquee: {
+          "0%": { transform: "translateX(0)" },
+          "100%": { transform: "translateX(-50%)" },
+        },
+      },
+      animation: {
+        marquee: "marquee 20s linear infinite",
+      },
+      fontFamily: {
+        geist: ["Geist", "sans-serif"],
+      },
+      colors: {
+        bgPrimary: "#FDF2D0",
+        bgSecondary: "#8B5E3C",
+        bgAccent: "#4A9660",
+        bgDark: "#121212",
+        bgLight: "#F9F9F9",
+        bgGray: "#6B6B6B",
+        txtPrimary: "#121212",
+        txtLight: "#F9F9F9",
+        txtAccent: "#8B5E3C",
+      },
+    },
   },
   plugins: [require("flowbite/plugin")],
 };
